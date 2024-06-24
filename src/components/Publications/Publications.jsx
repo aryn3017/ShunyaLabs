cfe330e9405fc207344a3edc837a2e6fdbcb2767
aryn3017 <aryn3017@gmail.com>
2024-06-24 18:43:33 +0530

    PublicationsPage 1st commit

--- src/components/Publications/Publications.jsx
+++ src/components/Publications/Publications.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Publications() {
   return (
-    <div className=' min-h-screen'>publications</div>
+    <div className=' min-h-screen'>This Page is not available, Come back later</div>
   )
 }
 
