b85ba62e7679235ecfed9d0226f176529e1a9183
aryn3017 <aryn3017@gmail.com>
2024-06-24 19:33:21 +0530

    ProfileHOVEReffect

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ function Profile() {
   return (
     <>
       <div className="   flex justify-center bg-gray-100 pb-10">
-        <a href="https://www.iitr.ac.in/~DM/Tarun_Sharma" className=" grid-flow-col shadow-xl mb-10 flex flex-row ssm:flex-col ssm:rounded-md ssm:mx-5 rounded-l-full rounded-r-full bg-gradient-to-r from-slate-400 to-gray-100 p-3">
+        <a href="https://www.iitr.ac.in/~DM/Tarun_Sharma" className=" grid-flow-col shadow-sm duration-100 mb-10 flex flex-row ssm:flex-col ssm:rounded-md ssm:mx-5 rounded-l-full rounded-r-full hover:shadow-2xl hover:-translate-y-1 bg-gradient-to-r from-slate-400 to-gray-100 p-3">
           <div className=" justify-center flex">
             <img src="./sir_icon.png" className=" max-w-40 rounded-full" />
           </div>
