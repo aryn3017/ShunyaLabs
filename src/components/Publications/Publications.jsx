b5adbb662bf996da6e065398dcb16b4dfb1f72c7
aryn3017 <aryn3017@gmail.com>
2024-06-27 23:41:39 +0530

    FIX: PUBLICATIONSpage

--- src/components/Publications/Publications.jsx
+++ src/components/Publications/Publications.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Publications() {
   return (
-    <div className=' min-h-screen'>This Page is not available, Come back later</div>
+    <div className=' min-h-screen'>
+      {/* <div className=' font-poppins text-center text-xl'>Market instrument for the first fuel and its role in decarbonizing Indian industrial production</div> */}
+      <iframe className=' w-full h-screen' src="https://www.sciencedirect.com/science/article/abs/pii/S0301421524001599?dgcid=author" frameborder="0"/>
+    </div>
   )
 }
 
